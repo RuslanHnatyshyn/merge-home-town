ea6669343641bfbc44fdf8280f2b6bac3cfb5a3d
Ruslan <gnatisin.ruslan1988@gmail.com>
2024-12-31 14:13:40 +0200

    added file vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        privacy: './src/pages/privacy.html',
+        terms: './src/pages/terms.html',
+      },
+    },
+  },
+});
